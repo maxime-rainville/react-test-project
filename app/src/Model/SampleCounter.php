2960f3c9afb71c1fa1ef68f4db47c3af81e8d10d
Marco Hermo <marco@silverstripe.com>
2021-07-22 09:30:51 +1200

    CSS class

--- app/src/Model/SampleCounter.php
+++ app/src/Model/SampleCounter.php
@@ -25,15 +25,15 @@ class SampleCounter extends DataObject
         $fields = parent::getCMSFields();
 
         $fields->addFieldsToTab('Root.Main', [
-            SSCounterField::create('InfoCounter', 'Info Count', 0)->addExtraClass('sscounter-info'),
-            SSCounterField::create('SuccessCounter', 'Success Count', 1)->addExtraClass('sscounter-success'),
-            SSCounterField::create('WarningCounter', 'Warning Count', 2)->addExtraClass('sscounter-warning'),
-            SSCounterField::create('DangerCounter', 'Danger Count', 3)->addExtraClass('sscounter-danger')
+            SSCounterField::create('InfoCounter', 'Info Count', 0)->addExtraClass('sscounter sscounter-info'),
+            SSCounterField::create('SuccessCounter', 'Success Count', 1)->addExtraClass('sscounter sscounter-success'),
+            SSCounterField::create('WarningCounter', 'Warning Count', 2)->addExtraClass('sscounter sscounter-warning'),
+            SSCounterField::create('DangerCounter', 'Danger Count', 3)->addExtraClass('sscounter sscounter-danger')
         ]);
 
         $fields->addFieldsToTab('Root.HappySad', [
-            SSCounterField::create('HappyCounter', 'Happy Count', 4)->addExtraClass('sscounter-happy'),
-            SSCounterField::create('SadCounter', 'Sad Count', 5)->addExtraClass('sscounter-sad')
+            SSCounterField::create('HappyCounter', 'Happy Count', 4)->addExtraClass('sscounter sscounter-happy'),
+            SSCounterField::create('SadCounter', 'Sad Count', 5)->addExtraClass('sscounter sscounter-sad')
         ]);
 
         return $fields;
